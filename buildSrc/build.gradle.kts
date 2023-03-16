35bab9d1b3c22e26e03e7bf757ab38b68de255ff
Giao The Cung <giaothe@gmail.com>
2023-03-16 09:13:43 +0100

    bruke baseimages fra ghcr.io

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,10 +3,5 @@ plugins {
 }
 
 repositories {
-    gradlePluginPortal()
-}
-
-dependencies {
-    implementation(kotlin("gradle-plugin"))
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:5.11.0")
+    mavenCentral()
 }
