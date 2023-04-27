99237d150f57f4e9f6992231dfec3f15a1589b08
Giao The Cung <giaothe@gmail.com>
2023-04-27 09:01:39 +0200

    Fikse bygg

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,4 +4,11 @@ plugins {
 
 repositories {
     mavenCentral()
+    gradlePluginPortal()
+}
+
+dependencies {
+    implementation(kotlin("gradle-plugin"))
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:5.11.0")
+
 }
