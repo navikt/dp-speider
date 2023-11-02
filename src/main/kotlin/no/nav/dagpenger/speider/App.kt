d3a0982b0b7914335036d41fb0366e449dfc1c66
Geir André Lund <geir.andre.lund@nav.no>
2023-11-02 13:08:06 +0100

    Synker mot helse-speider. Nytt: - Sender til hver 'ping' til hvert partisjon - Sender med info om alle instansene i app_status for å kunne raffinere varslingsmeldinger.

--- src/main/kotlin/no/nav/dagpenger/speider/App.kt
+++ src/main/kotlin/no/nav/dagpenger/speider/App.kt
@@ -16,11 +16,21 @@ import no.nav.helse.rapids_rivers.RapidApplication
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.rapids_rivers.River
 import no.nav.helse.rapids_rivers.asLocalDateTime
+import org.apache.kafka.clients.CommonClientConfigs
+import org.apache.kafka.clients.admin.AdminClient
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.Producer
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.config.SslConfigs
+import org.apache.kafka.common.security.auth.SecurityProtocol
+import org.apache.kafka.common.serialization.StringSerializer
 import org.slf4j.LoggerFactory
 import java.time.Duration
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit.SECONDS
+import java.util.Properties
+import java.util.UUID
 
 private val stateGauge =
     Gauge.build("dp_app_status", "Gjeldende status på apps")
@@ -32,6 +42,32 @@ private val logger = LoggerFactory.getLogger("no.nav.dagpenger.speider.App")
 fun main() {
     val env = System.getenv()
 
+    val props =
+        Properties().apply {
+            put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, env.getValue("KAFKA_BROKERS"))
+            put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SSL.name)
+            put(SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG, "")
+            put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, "jks")
+            put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, "PKCS12")
+            put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, env.getValue("KAFKA_TRUSTSTORE_PATH"))
+            put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, env.getValue("KAFKA_CREDSTORE_PASSWORD"))
+            put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, env.getValue("KAFKA_KEYSTORE_PATH"))
+            put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, env.getValue("KAFKA_CREDSTORE_PASSWORD"))
+        }
+    val adminClient = AdminClient.create(props)
+    val pingProducer = KafkaProducer(props, StringSerializer(), StringSerializer())
+
+    val topic = env.getValue("KAFKA_RAPID_TOPIC")
+    val partitionsCount =
+        adminClient.describeTopics(listOf(topic))
+            .allTopicNames()
+            .get()
+            .getValue(topic)
+            .partitions()
+            .size
+
+    logger.info("$topic consist of $partitionsCount partitions")
+
     val appStates = AppStates()
     var scheduledPingJob: Job? = null
     var statusPrinterJob: Job? = null
@@ -41,7 +77,7 @@ fun main() {
             object : RapidsConnection.StatusListener {
                 override fun onStartup(rapidsConnection: RapidsConnection) {
                     statusPrinterJob = GlobalScope.launch { printerJob(rapidsConnection, appStates) }
-                    scheduledPingJob = GlobalScope.launch { pinger(rapidsConnection) }
+                    scheduledPingJob = GlobalScope.launch { pinger(pingProducer, topic, partitionsCount) }
                 }
 
                 override fun onShutdown(rapidsConnection: RapidsConnection) {
@@ -61,7 +97,11 @@ fun main() {
                     packet: JsonMessage,
                     context: MessageContext,
                 ) {
-                    appStates.up(packet["app_name"].asText(), packet["instance_id"].asText(), packet["@opprettet"].asLocalDateTime())
+                    appStates.up(
+                        packet["app_name"].asText(),
+                        packet["instance_id"].asText(),
+                        packet["@opprettet"].asLocalDateTime(),
+                    )
                 }
 
                 override fun onError(
@@ -89,8 +129,13 @@ fun main() {
                     val pingTime = packet["ping_time"].asLocalDateTime()
                     val pongTime = packet["pong_time"].asLocalDateTime()
 
-                    logger.info("{}-{} svarte på ping etter {} sekunder", app, instance, SECONDS.between(pingTime, pongTime))
-                    appStates.up(app, instance, pongTime)
+                    logger.info(
+                        "{}-{} svarte på ping etter {} sekunder",
+                        app,
+                        instance,
+                        SECONDS.between(pingTime, pongTime),
+                    )
+                    appStates.ping(app, instance, pongTime)
                 }
 
                 override fun onError(
@@ -112,7 +157,11 @@ fun main() {
                     packet: JsonMessage,
                     context: MessageContext,
                 ) {
-                    appStates.down(packet["app_name"].asText(), packet["instance_id"].asText(), packet["@opprettet"].asLocalDateTime())
+                    appStates.down(
+                        packet["app_name"].asText(),
+                        packet["instance_id"].asText(),
+                        packet["@opprettet"].asLocalDateTime(),
+                    )
                 }
 
                 override fun onError(
@@ -123,8 +172,36 @@ fun main() {
                 }
             },
         )
+
+        River(this).apply {
+            validate { it.demandValue("@event_name", "application_stop") }
+            validate { it.requireKey("app_name", "instance_id") }
+            validate { it.require("@opprettet", JsonNode::asLocalDateTime) }
+        }.register(
+            object : River.PacketListener {
+                override fun onPacket(
+                    packet: JsonMessage,
+                    context: MessageContext,
+                ) {
+                    appStates.down(
+                        packet["app_name"].asText(),
+                        packet["instance_id"].asText(),
+                        packet["@opprettet"].asLocalDateTime(),
+                    )
+                }
+
+                override fun onError(
+                    problems: MessageProblems,
+                    context: MessageContext,
+                ) {
+                    logger.error("forstod ikke application_stop:\n${problems.toExtendedReport()}")
+                }
+            },
+        )
     }.start()
 }
+
+private fun Boolean.toInt() = if (this) 1 else 0
 
 private suspend fun CoroutineScope.printerJob(
     rapidsConnection: RapidsConnection,
@@ -134,24 +211,29 @@ private suspend fun CoroutineScope.printerJob(
         delay(Duration.ofSeconds(15))
         val threshold = LocalDateTime.now().minusMinutes(1)
         logger.info(appStates.reportString(threshold))
-        appStates.report(threshold).also { report ->
-            report.forEach { (app, state) ->
-                stateGauge.labels(app).set(state.toDouble())
-            }
+        appStates.report(threshold).onEach { (app, state) ->
+            stateGauge.labels(app).set(state.toInt().toDouble())
         }
         appStates.instances(threshold).also { report ->
             rapidsConnection.publish(
                 JsonMessage.newMessage(
+                    "app_status",
                     mapOf(
-                        "@event_name" to "app_status",
-                        "@opprettet" to LocalDateTime.now(),
                         "threshold" to threshold,
                         "states" to
-                            report.map {
-                                mapOf<String, Any>(
-                                    "app" to it.key,
-                                    "state" to it.value.first,
-                                    "last_active_time" to it.value.second,
+                            report.map { (appName, info) ->
+                                mapOf(
+                                    "app" to appName,
+                                    "state" to info.first.toInt(),
+                                    "last_active_time" to info.second,
+                                    "instances" to
+                                        info.third.map { (instanceId, lastActive, isUp) ->
+                                            mapOf(
+                                                "instance" to instanceId,
+                                                "last_active_time" to lastActive,
+                                                "state" to isUp.toInt(),
+                                            )
+                                        },
                                 )
                             },
                     ),
@@ -161,12 +243,20 @@ private suspend fun CoroutineScope.printerJob(
     }
 }
 
-private suspend fun CoroutineScope.pinger(rapidsConnection: RapidsConnection) {
-    val packet = JsonMessage.newMessage(mapOf("@event_name" to "ping"))
+private suspend fun CoroutineScope.pinger(
+    producer: Producer<String, String>,
+    topic: String,
+    partitionCount: Int,
+) {
     while (isActive) {
         delay(Duration.ofSeconds(30))
+        val packet = JsonMessage.newMessage("ping")
         packet["ping_time"] = LocalDateTime.now()
-        rapidsConnection.publish(packet.toJson())
+        // produces a ping to each partition
+        repeat(partitionCount) { partition ->
+            producer.send(ProducerRecord(topic, partition, UUID.randomUUID().toString(), packet.toJson()))
+        }
+        producer.flush()
     }
 }
 
@@ -192,6 +282,14 @@ internal class AppStates {
         App.up(states, app, instance, time)
     }
 
+    fun ping(
+        app: String,
+        instance: String,
+        time: LocalDateTime,
+    ) {
+        App.ping(states, app, instance, time)
+    }
+
     fun down(
         app: String,
         instance: String,
@@ -200,13 +298,11 @@ internal class AppStates {
         App.down(states, app, instance, time)
     }
 
-    fun report(threshold: LocalDateTime): Map<String, Int> {
+    fun report(threshold: LocalDateTime): Map<String, Boolean> {
         return instances(threshold).mapValues { it.value.first }
     }
 
-    fun instances(threshold: LocalDateTime): Map<String, Pair<Int, LocalDateTime>> {
-        return App.instances(states, threshold)
-    }
+    fun instances(threshold: LocalDateTime) = App.instances(states, threshold)
 
     fun reportString(threshold: LocalDateTime): String {
         return App.reportString(states, threshold)
@@ -217,7 +313,9 @@ internal class AppStates {
         private val instances: MutableList<Instance> = mutableListOf(),
         private var time: LocalDateTime,
     ) {
-        internal companion object {
+        private val downInstances: MutableList<Pair<String, LocalDateTime>> = mutableListOf()
+
+        companion object {
             fun up(
                 states: List<App>,
                 app: String,
@@ -226,28 +324,52 @@ internal class AppStates {
 
             fun up(
                 states: MutableList<App>,
-                app: String,
+                appName: String,
                 instance: String,
                 time: LocalDateTime,
             ) {
-                Instance.up(findOrCreateApp(states, app, time).instances, instance, time)
+                val app = findOrCreateApp(states, appName, time)
+                // re-active a downed app if we've received the application_up event
+                app.downInstances.removeAll { it.first == instance }
+                Instance.up(app.instances, instance, time)
+            }
+
+            fun ping(
+                states: MutableList<App>,
+                appName: String,
+                instance: String,
+                time: LocalDateTime,
+            ) {
+                val app = findOrCreateApp(states, appName, time)
+                if (app.downInstances.any { it.first == instance }) return // don't re-active a downed app
+                // remove all apps
+                app.downInstances.removeAll { it.second < LocalDateTime.now().minusHours(6) }
+                Instance.up(app.instances, instance, time)
             }
 
             fun down(
                 states: MutableList<App>,
-                app: String,
+                appName: String,
                 instance: String,
                 time: LocalDateTime,
             ) {
-                Instance.down(findOrCreateApp(states, app, time).instances, instance, time)
+                val app = findOrCreateApp(states, appName, time)
+                if (!Instance.down(app.instances, instance, time)) return
+                app.downInstances.add(instance to LocalDateTime.now())
             }
 
             fun instances(
                 states: List<App>,
                 threshold: LocalDateTime,
-            ): Map<String, Pair<Int, LocalDateTime>> {
-                return states.associateBy { it.name }
-                    .mapValues { (if (Instance.up(it.value.instances, threshold)) 1 else 0) to it.value.time }
+            ): Map<String, Triple<Boolean, LocalDateTime, List<Instance.Report>>> {
+                return states.associate { app ->
+                    app.name to
+                        Triple(
+                            Instance.up(app.instances, threshold),
+                            app.time,
+                            Instance.list(app.instances, threshold),
+                        )
+                }
             }
 
             fun reportString(
@@ -270,20 +392,35 @@ internal class AppStates {
                 states: MutableList<App>,
                 app: String,
                 time: LocalDateTime,
-            ) = states.firstOrNull { it.name == app }?.also {
-                it.time = maxOf(it.time, time)
-            } ?: App(app, mutableListOf(), time).also {
-                states.add(it)
+            ): App {
+                return states.firstOrNull { it.name == app }?.also {
+                    it.time = maxOf(it.time, time)
+                } ?: App(app, mutableListOf(), time).also {
+                    states.add(it)
+                }
             }
         }
     }
 
-    private class Instance(private val id: String, private var time: LocalDateTime) {
+    internal class Instance(private val id: String, private var time: LocalDateTime) {
         override fun toString(): String {
             return "$id: last active at ${time.format(timestampFormat)}"
         }
 
-        internal companion object {
+        fun up(threshold: LocalDateTime) = time >= threshold
+
+        data class Report(
+            val id: String,
+            val time: LocalDateTime,
+            val isUp: Boolean,
+        )
+
+        companion object {
+            fun list(
+                list: List<Instance>,
+                threshold: LocalDateTime,
+            ) = list.map { Report(it.id, it.time, it.up(threshold)) }
+
             fun up(
                 list: MutableList<Instance>,
                 instance: String,
@@ -298,14 +435,12 @@ internal class AppStates {
                 list: MutableList<Instance>,
                 instance: String,
                 time: LocalDateTime,
-            ) {
-                list.removeIf { it.id == instance && it.time < time }
-            }
+            ) = list.removeIf { it.id == instance && it.time <= time }
 
             fun up(
                 list: MutableList<Instance>,
                 threshold: LocalDateTime,
-            ) = list.any { it.time >= threshold }
+            ) = list.any { it.up(threshold) }
         }
     }
 }
